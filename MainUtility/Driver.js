727396c2ded08277084b2b61395270a6d30b7a2a
soham874 <soham874@gmail.com>
2020-12-25 01:34:33 +0530

    Added : Main menu and fixed bugs

--- MainUtility/Driver.js
+++ MainUtility/Driver.js
@@ -1,7 +1,42 @@
 const utility = require('./Utility.js')
 
+//main menu display function
+displayMainMenu = () => {
+    console.log("\n###### MAIN MENU ######   ")
+    console.log("<1> Add new data")
+    console.log("<2> View current data")
+    console.log("<3> Edit or delete previous data")
+    console.log("<4> Exit address book")
+    var choice = utility.takeUserInput("Please enter your choice")
+    console.log(choice)
+    switch (+choice) {
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            console.log("\nThank You for using address book!\n")
+            process.exit(0)
+        default:
+            console.log("\nWrong input. Please try again.")
+            displayMainMenu()
+    }
+}
+
 console.log("\nWelcome to Address Book!")
+var fileName = utility.takeUserInput("\nPlease enter file name to load from")
 
-var fileName = utility.takeUserInput("Please enter file name to load from.")
+utility.readPresentData(fileName)
+var addressbookdata = utility.retriveData()
+console.log(`Number of records present currently = ${addressbookdata.length}\n`)
 
-console.log(fileName)
+displayMainMenu()
--- MainUtility/Utility.js
+++ MainUtility/Utility.js
@@ -1,11 +1,36 @@
 const readLine = require('../../node_modules/readline-sync')
+const fs = require('fs');
+var addressBookData = []
 
 class Utility {
 
+    //displays passed message and takes input from user
     takeUserInput = (message) => {
-        let input = readLine.question(`\n${message} : `)
+        let input = readLine.question(`${message} : `)
         return input
+    }
+
+
+    //reads present data. Creates new file if file not present (sync)
+    readPresentData = (fileName) => {
+
+        try {
+            var data = fs.readFileSync(fileName)
+            addressBookData = JSON.parse(data);
+            console.log(`\nData loaded from pervious file successfully`)
         }
+        catch (err) {
+            let data = JSON.stringify([]);
+            fs.writeFileSync(fileName, data);
+            console.log("\nFile not found. Created new file");
+        }
+
+    }
+
+    //retrive current addressbook data
+    retriveData = () => {
+        return addressBookData
+    }
 
 }
 
